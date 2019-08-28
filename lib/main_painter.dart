bf128301ba7d5d88653bfc455b3cef1901e1601c
haipv <haipv@evolableasia.vn>
2019-08-28 08:55:05 +0700

    example manage state

--- lib/main_painter.dart
+++ lib/main_painter.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui show Image, Codec, instantiateImageCodec;
+
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,38 +20,78 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatefulWidget {
+
+  Home();
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  bool reDraw = false;
+  int tileLevelVertical = 3;
+  int tileLevelHorizontal = 2;
+  double tileWidth;
+  double tileHeight;
+  Rect rectSrc;
+  Rect rectDes;
+  ui.Image image;
+
+  Future<ui.Image> loatImage(String s) async {
+    ByteData data = await rootBundle.load(s);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List());
+    FrameInfo frameInfo = await codec.getNextFrame();
+    image = frameInfo.image;
+    return image;
+  }
+
   @override
   Widget build(BuildContext context) {
+    loatImage('images/smile.png').then((image) {
+      tileWidth = image.width / tileLevelVertical;
+      tileHeight = image.width / tileLevelHorizontal;
+      rectSrc = Rect.fromLTWH(0.0, 0.0, tileWidth, tileHeight);
+      rectDes = Rect.fromLTRB(
+          rectSrc.left, rectSrc.top, rectSrc.right, rectSrc.bottom);
+    });
     return Container(
-      child: CustomPaint(
-        foregroundPainter: MyPainter(),
+      child: GestureDetector(
+        onTapDown: changeRedraw,
+        child: CustomPaint(
+          foregroundPainter:
+              MyPainter(reDraw, image, rectSrc, rectDes),
+        ),
       ),
     );
+  }
+
+  changeRedraw(TapDownDetails details) {
+    reDraw = true;
+    setState(() {});
   }
 }
 
 class MyPainter extends CustomPainter {
+  bool reDraw;
+  ui.Image image;
+  Rect rectSrc;
+  Rect rectDes;
+
+  MyPainter(this.reDraw, this.image, this.rectSrc, this.rectDes);
+
   @override
   void paint(Canvas canvas, Size size) {
     print('painting.....');
-    Paint paint = Paint()
-      ..color = Colors.red
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round;
+    if (image != null) {
+      canvas.drawImageRect(image, rectSrc, rectDes, Paint());
+    }
 
-    Offset center = new Offset(size.width / 2, size.height / 2);
-    Rect rect = Rect.fromLTWH(0, 0, 50, 50);
-    canvas.drawRect(rect, paint);
+    print('END: paint');
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     print('rpainting.....');
-    return true;
+    return false;
   }
 }
